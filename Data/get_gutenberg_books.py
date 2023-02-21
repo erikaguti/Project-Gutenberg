d5c5195f3623691d3b158c61b8eccd7aa3f8b73a
4Freye <4freye@gmail.com>
2023-02-21 20:38:48 +0100

    added some things, fixed some others

--- Data/get_gutenberg_books.py
+++ Data/get_gutenberg_books.py
@@ -1,14 +1,23 @@
 # %%
 import pandas as pd
 from API import get_book, baseurl
-dataset = pd.read_csv('american19thcenturyLit.csv')
+dataset = pd.read_csv('goodreads/getbooks.csv')
 
 downloads = []
+
 for i in range(len(dataset)):
-    downloaded = get_book(dataset.loc[i,'book'], dataset.loc[i,'author'], baseurl)
-    downloads.append(downloaded.extend(dataset.loc[i, 'book']))
+    downloaded = get_book(dataset.loc[i,'book_title'], dataset.loc[i,'author'], baseurl)
+    if type(downloaded)== dict :
+        downloaded.update({'book_id':dataset.loc[i, 'book_id']})
+        downloads.append(downloaded)
+    else:
+        continue
 
 pd.DataFrame(downloads).to_csv('gutenbergDownloads.csv')
 
 # %%
+gutenbergDownloads = pd.read_csv('gutenbergDownloads.csv')
+gutenbergDownloads[~gutenbergDownloads.title.str.contains('Vol')].to_csv('gutenbergDownloads.csv')
+
+
 
